51392311b5bc7fe66277fb2458dc04e2f1b36bbd
Pepe Valdivia <jvaldivia@softweb.pe>
2019-01-01 21:58:26 -0500

    Navegación de concurso

--- public/competition/templates/sign_in_template.js
+++ public/competition/templates/sign_in_template.js
@@ -1,4 +1,56 @@
 var signInTemplate = _.template(`
+  <div class="col-md-12">
+    <h1 class="text-uppercase text-md-center font-weight-bold color-primary">Ficha de Inscripción</h1>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="form-group">
+          <label for="txtDni">DNI</label>
+          <input type="text" class="form-control" id="txtDni" aria-describedby="emailHelp" placeholder="Ingrese su DNI">
+          <small id="txtDniHelp" class="form-text text-muted"></small>
+        </div>
+      </div>
+      <div class="col-md-12">
+        <div class="form-group">
+          <label for="txtName">Nombre Completo</label>
+          <input type="text" class="form-control" id="txtName" aria-describedby="emailHelp" placeholder="" disabled>
+          <small id="txtNameHelp" class="form-text text-muted"></small>
+        </div>
+      </div>
+      <div class="col-md-12">
+        <div class="form-group">
+          <label for="txtDni">Sede</label>
+          <select class="custom-select" id="slcBranch" disabled>
+            <option selected>Seleccione una sede...</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+          <small id="txtBranchHelp" class="form-text text-muted"></small>
+        </div>
+      </div>
+      <div class="col-md-12">
+        <div class="form-group">
+          <label for="txtAddress">Dirección</label>
+          <textarea class="form-control" rows="2" id="txtAddress" placeholder="Ingrese su dirección" disabled></textarea>
+          <small id="txtAddressHelp" class="form-text text-muted"></small>
+        </div>
+      </div>
+      <div class="col-md-12">
+        <div class="form-group">
+          <label for="txtPhone">Teléfono</label>
+          <input type="text" class="form-control" id="txtPhone" aria-describedby="emailHelp" placeholder="Ingrese su teléfono" disabled>
+          <small id="txtPhoneHelp" class="form-text text-muted"></small>
+        </div>
+      </div>
+      <div class="col-md-12">
+        <div class="form-group">
+          <label for="txtEmail">Email</label>
+          <input type="email" class="form-control" id="txtEmail" aria-describedby="emailHelp" placeholder="Ingrese su correo" disabled>
+          <small id="txtEmailHelp" class="form-text text-muted"></small>
+        </div>
+      </div>
+    </div>
+  </div>
   <div class="col-md-12 text-md-center links">
     <a href="<%= BASE_URL %>competition/#/" class="link">INICIO</a>
     <a href="<%= BASE_URL %>competition/#/upload" class="link">SIGUIENTE</a>
--- public/competition/templates/upload_template.js
+++ public/competition/templates/upload_template.js
@@ -1,4 +1,41 @@
 var uploadTemplate = _.template(`
+  <div class="col-md-12">
+    <h1 class="text-uppercase text-md-center font-weight-bold color-primary">Ficha de Inscripción</h1>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="form-group">
+          <label for="txtTitle">Título de la Obra</label>
+          <input type="text" class="form-control" id="txtTitle" aria-describedby="emailHelp" placeholder="Ingrese el titulo de la obra" disabled>
+          <small id="txtTitleHelp" class="form-text text-muted"></small>
+        </div>
+      </div>
+      <div class="col-md-12">
+        <div class="form-group">
+          <label for="txtDescription">Breve Descripción de la Obra</label>
+          <textarea class="form-control" rows="5" id="txtDescription" placeholder="Descripción de la foto a subir" disabled></textarea>
+          <small id="txtDescriptionHelp" class="form-text text-muted"></small>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <button type="button" class="btn btn-link btn-large">
+          <i class="fa fa-search" aria-hidden="true"></i>
+          Seleccionar Archivo
+        </button>
+      </div>
+      <div class="col-md-6">
+        <button type="button" class="btn btn-link btn-large">
+          <i class="fa fa-cloud-upload" aria-hidden="true"></i>
+          Subir Archivo
+        </button>
+      </div>
+      <div class="col-md-12">
+        <button type="button" class="btn btn-large btn-primary">
+          <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
+          Enviar Inscripción
+        </button>
+      </div>
+    </div>
+  </div>
   <div class="col-md-12 text-md-center links">
     <a href="<%= BASE_URL %>competition/#/sign_in" class="link">ATRAS</a>
   </div>
